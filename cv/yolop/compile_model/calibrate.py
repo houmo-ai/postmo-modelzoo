dca136822afd4d031c4f125f20e2780229de91ec
wei.xiong <wei.xiong@houmo.ai>
2023-06-02 19:35:18 +0800

    TLC-835: yolop quant->compile->inference 192x320 performance sample
    Change-Id: I93214ccde35d49f403ff5fc0734a73887f06615c

---
+++ cv/yolop/compile_model/calibrate.py
@@ -0,0 +1,56 @@
+import os
+
+import onnx
+import torch
+import torchvision.transforms as transforms
+from hmquant.api import quant_single_input_onnx_network
+from hmquant.api import quant_single_onnx_network
+from hmquant.configs.api_config import BaseHoumoConfig
+from hmquant.tools.dataset.preprocess.transform import RGB2YUV
+from hmquant.tools.dataset.preprocess.transform import ToTensorNotNormal
+from torchvision.datasets.folder import pil_loader
+
+
+def calibrate():
+    env_dict = os.environ
+    onnx_model_path = os.path.join(
+        env_dict.get('MODEL_PATH'), 'yolop.onnx',
+    )
+
+    calib_dataset = [torch.randint(0, 200, (1, 3, 192, 320))]
+
+    quanttool_config = {
+        'inputs_cfg': {
+            'ALL': {
+                'data_format': 'RGB',
+                'first_layer_weight_denorm_mean': [0.485, 0.456, 0.406],
+                'first_layer_weight_denorm_std': [0.229, 0.224, 0.225],
+                'resizer_crop': {'top': 0, 'left': 0, 'height': 192, 'width': 320},
+                'resizer_resize': {
+                    'height': 192,
+                    'width': 320,
+                    'align_corners': False,
+                    'method': 'bilinear',
+                },
+                'toYUV_format': 'YUV422',
+            },
+        },
+        'graph_opt_cfg': {},
+    }
+    sequencer = quant_single_onnx_network(
+        quanttool_config, calib_dataset, onnx_model_path, device='cpu',
+    )
+    # Remove quanttool only op
+    remove_node_ids = sequencer.find_node_by_classes(['ToYuv', 'HMQuantize'])
+    if len(remove_node_ids) > 0:
+        print(f'Remove {remove_node_ids}')
+        sequencer.remove_nodes(remove_node_ids)
+    sequencer.save_onnx(
+        'quant_yolop.onnx',
+        save_out_tensor=False,
+        save_params_npy=True,
+    )
+
+
+if __name__ == '__main__':
+    calibrate()
--- cv/yolop/perf.sh
+++ cv/yolop/perf.sh
@@ -17,5 +17,14 @@ cd compile_model
 ./run.sh --batch 1
 cd ..
 
-cd inferece_model
-./run.sh
+cd ../../utils/tcimexec/
+./build.sh
+PATH="$(pwd):${PATH}"
+export PATH
+cd -
+
+if [ -c /dev/hmcl_feature_in ]; then
+  export HDPL_PLATFORM=ASIC
+fi
+
+tcimexec --model "${SCRIPT_DIR}/compile_model/yolop" --iterations 1000
