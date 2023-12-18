109b18e2e02fdd501fb4056fa09e473b30739a6b
yan.cao <yan.cao@houmo.ai>
2023-12-18 14:34:25 +0800

    TLC-1709: add tests/resnet50 for product test
    Change-Id: If5025e047ede3cbde3d6ae8fdaf9b76e93a7a1fd

---
+++ tests/resnet50/get_model.py
@@ -0,0 +1,36 @@
+import os
+import onnx
+import argparse
+
+def get_args() -> argparse.Namespace:
+    """Parse commandline."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--type',
+        dest='model_type',
+        type=str,
+        default='all',
+        help='model_type to get',
+    )
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    args = get_args()
+    if args.model_type == "raw" or args.model_type == "all":
+        # import torch
+        # import torchvision
+        # model = torchvision.models.resnet50(pretrained=True)
+        # dummy_input = torch.rand((1, 3, 224, 224))
+        # torch.onnx.export(model, dummy_input, 'resnet50.onnx', export_params=True, opset_version=13)
+        if not os.path.exists("resnet50.onnx"):
+            os.system('wget http://10.10.1.53:8082/artifactory/toolchain/release/models/resnet50/resnet50.onnx')
+
+    if args.model_type == "quant" or args.model_type == "all":
+        if not os.path.exists("output/H30/result/hmquant_resnet50_with_act.onnx"):
+            if not os.path.exists("resnet50_golden_20231218.zip"):
+                os.system('wget http://10.10.1.53:8082/artifactory/toolchain/release/models/resnet50/resnet50_golden_20231218.zip')
+            os.system('mkdir -p output/H30/result')
+            os.system('unzip -d output/H30/result resnet50_golden_20231218.zip')
+
+    # onnx.utils.extract_model("resnet50.onnx", "resnet50_clip.onnx", input_names=[''], output_names=[''], check_model=True)
