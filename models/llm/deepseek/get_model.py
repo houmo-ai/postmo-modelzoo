658e311a8f04507a0d753bd5d45450a6473f81c8
yan.cao <yan.cao@houmo.ai>
2025-02-13 21:03:36 +0800

    HMSW-1297: modelzoo add deepseek
    Change-Id: Ib00212ad2a40aefbd9c9e052aab5a104bd69baaa

---
+++ models/llm/deepseek/get_model.py
@@ -0,0 +1,59 @@
+import os
+import onnx
+import argparse
+from hmassist.utils.utils import get_file_from_jfrog
+
+HOUMO_TARGET = os.getenv('HOUMO_TARGET', 'houmo')
+
+
+def get_args() -> argparse.Namespace:
+    """Parse commandline."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--type',
+        dest='model_type',
+        type=str,
+        default='all',
+        help='which resource to get, choise in [dataset, raw, quant, hmm, all]',
+    )
+    parser.add_argument(
+        '--quant_model_dir',
+        dest='quant_model_dir',
+        type=str,
+        default=os.path.join('output', HOUMO_TARGET, 'hmquant'),
+        help='where to save quant_model',
+    )
+    parser.add_argument(
+        '--model_dir',
+        dest='model_dir',
+        type=str,
+        default='',
+        help='where to save downloaded model',
+    )
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = get_args()
+    quant_model_dir = args.quant_model_dir
+    model_type = args.model_type
+    model_dir = args.model_dir
+    DATASETS_PATH = os.getenv('DATASETS_PATH', '.')
+    MODEL_PATH = os.getenv('MODEL_PATH', '.')
+    wiki_path = "models/datasets/wikitext-2-raw-v1.zip"
+    quant_path = "models/deepseek/hmquant_deepseek_256_4096_20250225.zip"
+    hmm_path = "models/deepseek/hmm_4cores_deepseek_256_4096_20250226.zip"
+
+    if model_type == "dataset" or model_type == "all":
+        get_file_from_jfrog(wiki_path, model_dir, DATASETS_PATH)
+
+    if model_type == "raw" or model_type == "all":
+        from modelscope import snapshot_download
+        snapshot_download('deepseek-ai/DeepSeek-R1-Distill-Qwen-7B', local_dir='DeepSeek-R1-Distill-Qwen-7B')
+
+    if model_type == "quant" or model_type == "all":
+        get_file_from_jfrog(quant_path, model_dir, quant_model_dir)
+
+    if model_type == "hmm" or model_type == "all":
+        get_file_from_jfrog(hmm_path, model_dir, os.path.join('output', HOUMO_TARGET))
---
+++ models/llm/deepseek/ptq.py
@@ -0,0 +1,74 @@
+import torch, argparse,os
+import hmquant.llm.llm_utils  as utils
+from hmquant.llm.llm_api import QwenQuantPipline
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+DATASETS_PATH = os.getenv('DATASETS_PATH', '')
+HOUMO_TARGET = os.getenv('HOUMO_TARGET', '')
+
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1',""):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Quant DeepSeek")
+    parser.add_argument("--model", type=str, default="DeepSeek-R1-Distill-Qwen-7B", help="path to model")
+    # 1. quant
+    parser.add_argument("--quant_config", type=str, default="quant_config.py")
+    parser.add_argument("--n_calib", type=int, default=16)
+    # 2. export
+    parser.add_argument("--model_name", type=str, default="deepseek")
+    parser.add_argument("--save_path",type=str,default=f"output/{HOUMO_TARGET}/hmquant")
+    parser.add_argument("--prefill_shape", type=int, nargs='+', default=[4, 64], help="List of integers for prefill shape")
+    parser.add_argument("--cache_len", type=int, default=4096)
+    parser.add_argument("--multi_batch",action="store_true",default=False,help="weather use multi batch for export")
+    # 3. others
+    parser.add_argument("--wikitext_local",type=str,default=os.path.join(DATASETS_PATH,"wikitext-2-raw-v1"),help="if has local wikitext, set it here")
+    parser.add_argument("--eval_ppl",action="store_true",default=False)
+
+    """  args below are for debug, please not used """
+    parser.add_argument("--blocks", default=28, type=int)
+    parser.add_argument("--decoder_shape", type=int, nargs='+', default=[1, 1], help="List of integers for decoder shape")
+    parser.add_argument("--gptq",type=str2bool,default=False,help="weather use gptq to quant weight") # boost precision
+    parser.add_argument("--cache_2_input",type=str2bool,default=True)
+    parser.add_argument("--rotate_ov",type=str2bool,default=True,help="weather rotate o_proj and v_proj")
+    parser.add_argument("--rotate_pre_rope",type=str2bool,default=False,help="weather rotate acts before rope")
+    parser.add_argument("--rotate_post_rope",type=str2bool,default=False,help="weather rotate acts after rope")
+    parser.add_argument("--use_klt",type=str2bool,default=True,help="weather use klt for rotation")
+    parser.add_argument("--compile_mode",type=str2bool,default=False,help="weather show convert err")
+    """  args above are for debug, please not used """
+    args = parser.parse_args()
+    if args.multi_batch:
+        args.decoder_shape = [4,1]
+    else:
+        args.decoder_shape = [1,1]
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    model, tokenizer = AutoModelForCausalLM.from_pretrained(args.model), AutoTokenizer.from_pretrained(args.model)
+    quant_pipline = QwenQuantPipline()
+    # 1. quant model
+    qmodel = quant_pipline.quant_llm(model, tokenizer, args=args)
+    if args.eval_ppl:
+        utils.eval_ppl(qmodel, tokenizer, disk_file=args.wikitext_local)
+        ques_res = qmodel.stream_chat(tokenizer,"hello")
+    # 2. export model
+    quant_pipline.export_llm(qmodel, tokenizer, args)
+
+    # 3. chat
+    if not args.cache_2_input:
+        while True:
+            prompt = input("\n你的问题：")
+            prompt = prompt.replace("\\n", "\n")
+            quant_pipline.chat(prompt,args)
+
+    # 4. generate golden
+    quant_pipline.generate_golden(args, save_path=args.save_path, model_name=args.model_name)
