dd5a25d521d1a0a8275cc9ce0f34d3227b685223
yan.cao <yan.cao@houmo.ai>
2025-06-28 22:58:02 +0800

    HMSW-1990: update pack.py
    Change-Id: I7a8a37c4de17f9fc431ea6fd730ca17815939d9a

--- pack.py
+++ pack.py
@@ -5,7 +5,7 @@ version = ""
 if len(sys.argv) > 1:
     version = sys.argv[1]
 include_list = ["data", "hmassist", "hmodel", "models", "utils/common", "utils/tcim_perf", "utils/release.cmake",
-                "requirements.txt", "env.sh", "benchmark.yml", "README.md"]
+                "requirements*.txt", "env.sh", "benchmark.yml", "README.md"]
 exclude_list = ['models/backbone/vit']
 dir_name = os.path.basename(os.getcwd())
 
@@ -28,18 +28,19 @@ for file in exclude_list:
 # print(cmd)
 # os.system(cmd)
 
-# tar
 postfix = ""
 if version != "":
     postfix = "_" + version
-cmd = f"cd .. && tar -czf {dir_name}{postfix}.tar.gz {exclude_cmd} {include_cmd}"
-print(cmd)
-os.system(cmd)
-print(f"tar {dir_name}{postfix} done.")
 
-# houmo-examples
-cmd = f"cd {dir_name} && mv examples houmo-examples && zip houmo-examples{postfix}.zip -r houmo-examples && mv houmo-examples{postfix}.zip .."
+# pack houmo-examples
+cmd = f"mv examples houmo-examples && zip houmo-examples{postfix}.zip -r houmo-examples && mv houmo-examples{postfix}.zip .."
 print(cmd)
 
 os.system(cmd)
 print(f"zip houmo-examples{postfix} done.")
+
+# pack modelzoo
+cmd = f"cd .. && tar -czf {dir_name}{postfix}.tar.gz {exclude_cmd} {include_cmd}"
+print(cmd)
+os.system(cmd)
+print(f"tar {dir_name}{postfix} done.")
