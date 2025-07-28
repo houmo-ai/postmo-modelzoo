0958f019bd68f7bcdd580f323fe70be287066873
weiguo.xing <weiguo.xing@houmo.ai>
2025-07-28 17:22:05 +0800

    HMSW-2093: modify paddding value 114
    Change-Id: If947dd04125362931e6300468c5cfe69055204dd

--- examples/inferences/yolov5s/yolov5s.cc
+++ examples/inferences/yolov5s/yolov5s.cc
@@ -19,12 +19,10 @@ namespace fs = std::filesystem;
 #ifdef ENABLE_ORT
 #include "onnxruntime_cxx_api.h"
 #endif
-#include "tcim/tcim_runtime.h"
-
 #include "imageproc.hpp"
+#include "tcim/tcim_runtime.h"
 #include "threads.hpp"
 #include "utils.hpp"
-
 
 struct Box {
   int x1{0};
@@ -51,8 +49,8 @@ struct Box {
 
 typedef struct {
   float conf{0.0f};
-  int cls{-1};  // class id
-  std::string name; // class name
+  int cls{-1};       // class id
+  std::string name;  // class name
   Box box;
   float mask[32]{};
 } Detection;
@@ -64,7 +62,7 @@ typedef struct {
   int stride{0};
 } DetectOutput;
 
-float bbox_overlap(const Box &vi, const Box &vo) {
+float bbox_overlap(const Box& vi, const Box& vo) {
   int xx1 = std::max(vi.x1, vo.x1);
   int yy1 = std::max(vi.y1, vo.y1);
   int xx2 = std::min(vi.x2, vo.x2);
@@ -73,134 +71,136 @@ float bbox_overlap(const Box &vi, const Box &vo) {
   int h = std::max(0, yy2 - yy1);
   int area = w * h;
   float dist = float(area) / float((vi.x2 - vi.x1) * (vi.y2 - vi.y1) +
-                                    (vo.y2 - vo.y1) * (vo.x2 - vo.x1) - area);
+                                   (vo.y2 - vo.y1) * (vo.x2 - vo.x1) - area);
   return dist;
 }
 
-int non_max_suppression(std::vector<Detection> &detections, const float iou_threshold) {
+int non_max_suppression(std::vector<Detection>& detections,
+                        const float iou_threshold) {
   // sort
   std::sort(detections.begin(), detections.end(),
-            [](const Detection &d1, const Detection &d2) { return d1.conf > d2.conf; });
+            [](const Detection& d1, const Detection& d2) {
+              return d1.conf > d2.conf;
+            });
 
   // nms
   std::vector<Detection> keep_detections;
-  bool *suppressed = new bool[detections.size()];
+  bool* suppressed = new bool[detections.size()];
   memset(suppressed, 0, sizeof(bool) * detections.size());
   const int num_detections = detections.size();
   for (int i = 0; i < num_detections; ++i) {
-      if (suppressed[i])
-          continue;
-      keep_detections.emplace_back(detections[i]);
-      for (int j = i + 1; j < num_detections; ++j) {
-          if (suppressed[j])
-              continue;
-          float iou = bbox_overlap(detections[i].box, detections[j].box);
-          if (iou > iou_threshold)
-              suppressed[j] = true;
-      }
+    if (suppressed[i]) continue;
+    keep_detections.emplace_back(detections[i]);
+    for (int j = i + 1; j < num_detections; ++j) {
+      if (suppressed[j]) continue;
+      float iou = bbox_overlap(detections[i].box, detections[j].box);
+      if (iou > iou_threshold) suppressed[j] = true;
+    }
   }
   keep_detections.swap(detections);
-  delete[]suppressed;
+  delete[] suppressed;
 
   return 0;
 }
 
-cv::Mat letterbox(cv::Mat &img, int height, int width) {
+cv::Mat letterbox(cv::Mat& img, int height, int width) {
   float scale;
   int resize_rows;
   int resize_cols;
-  if ((height * 1.0 / img.rows) < (width * 1.0 / img.cols))
-  {
-      scale = height * 1.0 / img.rows;
-  }
-  else
-  {
-      scale = width * 1.0 / img.cols;
+  if ((height * 1.0 / img.rows) < (width * 1.0 / img.cols)) {
+    scale = height * 1.0 / img.rows;
+  } else {
+    scale = width * 1.0 / img.cols;
   }
   resize_cols = int(scale * img.cols);
   resize_rows = int(scale * img.rows);
 
   cv::resize(img, img, cv::Size(resize_cols, resize_rows));
   // Generate a gray image for letterbox using opencv
-  cv::Mat img_new(width, height, CV_8UC3, cv::Scalar(0, 0, 0));
   int top = (height - resize_rows) / 2;
   int bot = (height - resize_rows + 1) / 2;
   int left = (width - resize_cols) / 2;
   int right = (width - resize_cols + 1) / 2;
   // Letterbox filling
-  cv::copyMakeBorder(img, img_new, top, bot, left, right, cv::BORDER_CONSTANT, cv::Scalar(0, 0, 0));
+  cv::Mat img_new;
+  cv::copyMakeBorder(img, img_new, top, bot, left, right, cv::BORDER_CONSTANT,
+                     cv::Scalar(114, 114, 114));
 
   return img_new;
 }
-
 
 class YoloV5 {
  public:
   // convert the coordinates of the archor box
   void convert_box_coords(float cx, float cy, float w, float h, Box& box) {
-      float scale = (float)input_sizes_[0] / std::max(img_rows_, img_cols_);
-      float pad_h = (input_sizes_[0] - img_rows_ * scale) * 0.5f;
-      float pad_w = (input_sizes_[1] - img_cols_ * scale) * 0.5f;
+    float scale = (float)input_sizes_[0] / std::max(img_rows_, img_cols_);
+    float pad_h = (input_sizes_[0] - img_rows_ * scale) * 0.5f;
+    float pad_w = (input_sizes_[1] - img_cols_ * scale) * 0.5f;
 
-      // scale coords
-      int x1 = (int)((cx - w * 0.5f - pad_w) / scale);
-      int y1 = (int)((cy - h * 0.5f - pad_h) / scale);
-      int x2 = (int)((cx + w * 0.5f - pad_w) / scale);
-      int y2 = (int)((cy + h * 0.5f - pad_h) / scale);
+    // scale coords
+    int x1 = (int)((cx - w * 0.5f - pad_w) / scale);
+    int y1 = (int)((cy - h * 0.5f - pad_h) / scale);
+    int x2 = (int)((cx + w * 0.5f - pad_w) / scale);
+    int y2 = (int)((cy + h * 0.5f - pad_h) / scale);
 
-      // clip
-      box.x1 = x1 < 0 ? 0 : x1;
-      box.y1 = y1 < 0 ? 0 : y1;
-      box.x2 = x2 >= img_cols_? img_cols_ - 1 : x2;
-      box.y2 = y2 >= img_rows_ ? img_rows_ - 1 : y2;
+    // clip
+    box.x1 = x1 < 0 ? 0 : x1;
+    box.y1 = y1 < 0 ? 0 : y1;
+    box.x2 = x2 >= img_cols_ ? img_cols_ - 1 : x2;
+    box.y2 = y2 >= img_rows_ ? img_rows_ - 1 : y2;
   }
 
-  // calculate the score of each anchor box through cpp code 
-  int calculate_detections(std::vector<DetectOutput> outputs, std::vector<Detection>& detections) {
-    static float anchors[18] = {10, 13, 16, 30, 33, 23, 30, 61, 62, 45, 59, 119, 116, 90, 156, 198, 373, 326};
+  // calculate the score of each anchor box through cpp code
+  int calculate_detections(std::vector<DetectOutput> outputs,
+                           std::vector<Detection>& detections) {
+    static float anchors[18] = {10, 13, 16,  30,  33, 23,  30,  61,  62,
+                                45, 59, 119, 116, 90, 156, 198, 373, 326};
     int anchor_num = 3;
     int anchor_group;
 
-    for (auto &output : outputs) {
+    for (auto& output : outputs) {
       int stride = output.stride;
       float* feat = output.data;
       int feat_w = input_sizes_[1] / stride;
       int feat_h = input_sizes_[0] / stride;
-      if (stride == 8) anchor_group = 1;
-      else if (stride == 16) anchor_group = 2;
-      else if (stride == 32) anchor_group = 3;
+      if (stride == 8)
+        anchor_group = 1;
+      else if (stride == 16)
+        anchor_group = 2;
+      else if (stride == 32)
+        anchor_group = 3;
       else {
         printf("[error] wrong stride: %d\n", stride);
         return -1;
       }
-      for (int h = 0; h <= feat_h - 1; h++)
-      {
-        for (int w = 0; w <= feat_w - 1; w++)
-        {
-          for (int a = 0; a <= anchor_num - 1; a++)
-          {
+      for (int h = 0; h <= feat_h - 1; h++) {
+        for (int w = 0; w <= feat_w - 1; w++) {
+          for (int a = 0; a <= anchor_num - 1; a++) {
             // process class score
             int class_index = 0;
             float class_score = -1.0;
-            for (int s = 0; s <= num_classes_ - 1; s++)
-            {
-                float score = feat[a * feat_w * feat_h * (num_classes_ + 5) + h * feat_w * (num_classes_ + 5) + w * (num_classes_ + 5) + s + 5];
-                if (score < conf_threshold_) continue;
-                if (score > class_score)
-                {
-                    class_index = s;
-                    class_score = score;
-                }
+            for (int s = 0; s <= num_classes_ - 1; s++) {
+              float score = feat[a * feat_w * feat_h * (num_classes_ + 5) +
+                                 h * feat_w * (num_classes_ + 5) +
+                                 w * (num_classes_ + 5) + s + 5];
+              if (score < conf_threshold_) continue;
+              if (score > class_score) {
+                class_index = s;
+                class_score = score;
+              }
             }
             // process box score
-            float box_score = feat[a * feat_w * feat_h * (num_classes_ + 5) + (h * feat_w) * (num_classes_ + 5) + w * (num_classes_ + 5) + 4];
+            float box_score = feat[a * feat_w * feat_h * (num_classes_ + 5) +
+                                   (h * feat_w) * (num_classes_ + 5) +
+                                   w * (num_classes_ + 5) + 4];
             // calculate final confidence
             float final_score = box_score * class_score;
 
             // filter out boxes with low confidence
-            if (final_score >= conf_threshold_)
-            {
-              int loc_idx = a * feat_h * feat_w * (num_classes_ + 5) + h * feat_w * (num_classes_ + 5) + w * (num_classes_ + 5);
+            if (final_score >= conf_threshold_) {
+              int loc_idx = a * feat_h * feat_w * (num_classes_ + 5) +
+                            h * feat_w * (num_classes_ + 5) +
+                            w * (num_classes_ + 5);
               float dx = feat[loc_idx + 0];
               float dy = feat[loc_idx + 1];
               float dw = feat[loc_idx + 2];
@@ -237,7 +237,8 @@ class YoloV5 {
     Ort::SessionOptions session_options;
     session_options.SetIntraOpNumThreads(4);  // set thread num
     // turn on graph optimization
-    session_options.SetGraphOptimizationLevel(GraphOptimizationLevel::ORT_ENABLE_ALL);
+    session_options.SetGraphOptimizationLevel(
+        GraphOptimizationLevel::ORT_ENABLE_ALL);
     // load postprocess model
     Ort::Session session(env, model_path.c_str(), session_options);
 
@@ -247,10 +248,10 @@ class YoloV5 {
     std::vector<const char*> input_names_chr;
     std::vector<std::vector<int64_t>> input_shapes(num_inputs);
     for (size_t i = 0; i < num_inputs; i++) {
-        input_names_chr.emplace_back(input_names[i].c_str());
-        Ort::TypeInfo input_type_info = session.GetInputTypeInfo(i);
-        auto input_tensor_info = input_type_info.GetTensorTypeAndShapeInfo();
-        input_shapes[i] = input_tensor_info.GetShape();
+      input_names_chr.emplace_back(input_names[i].c_str());
+      Ort::TypeInfo input_type_info = session.GetInputTypeInfo(i);
+      auto input_tensor_info = input_type_info.GetTensorTypeAndShapeInfo();
+      input_shapes[i] = input_tensor_info.GetShape();
     }
     // get output info
     size_t num_outputs = session.GetOutputCount();
@@ -258,34 +259,26 @@ class YoloV5 {
     std::vector<const char*> output_names_chr;
     std::vector<size_t> output_element_cnt(num_outputs);
     for (size_t i = 0; i < num_outputs; i++) {
-        output_names_chr.emplace_back(output_names[i].c_str());
-        Ort::TypeInfo output_type_info = session.GetOutputTypeInfo(i);
-        auto output_tensor_info = output_type_info.GetTensorTypeAndShapeInfo();
-        output_element_cnt[i] = output_tensor_info.GetElementCount();
+      output_names_chr.emplace_back(output_names[i].c_str());
+      Ort::TypeInfo output_type_info = session.GetOutputTypeInfo(i);
+      auto output_tensor_info = output_type_info.GetTensorTypeAndShapeInfo();
+      output_element_cnt[i] = output_tensor_info.GetElementCount();
     }
 
     // create input tensors
     std::vector<Ort::Value> input_tensors;
-    auto memory_info = Ort::MemoryInfo::CreateCpu(OrtArenaAllocator, OrtMemTypeDefault);
+    auto memory_info =
+        Ort::MemoryInfo::CreateCpu(OrtArenaAllocator, OrtMemTypeDefault);
     for (size_t i = 0; i < num_inputs; i++) {
-        input_tensors.emplace_back(Ort::Value::CreateTensor<float>(
-            memory_info,
-            md_outputs[i].data,
-            md_outputs[i].data_size,
-            input_shapes[i].data(),
-            input_shapes[i].size()
-        ));
+      input_tensors.emplace_back(Ort::Value::CreateTensor<float>(
+          memory_info, md_outputs[i].data, md_outputs[i].data_size,
+          input_shapes[i].data(), input_shapes[i].size()));
     }
 
     // execute postprocess inference
     auto output_tensors = session.Run(
-        Ort::RunOptions{nullptr},
-        input_names_chr.data(),
-        input_tensors.data(),
-        input_tensors.size(),
-        output_names_chr.data(),
-        output_names.size()
-    );
+        Ort::RunOptions{nullptr}, input_names_chr.data(), input_tensors.data(),
+        input_tensors.size(), output_names_chr.data(), output_names.size());
     if (output_tensors.size() == 0) {
       return -1;
     }
@@ -308,11 +301,11 @@ class YoloV5 {
       int class_id = 0;
       float class_score = 0.0f;
       for (int j = 0; j < num_classes_; ++j) {
-          float prob = box[5 + j];
-          if (prob > class_score) {
-              class_score = prob;
-              class_id = j;
-          }
+        float prob = box[5 + j];
+        if (prob > class_score) {
+          class_score = prob;
+          class_id = j;
+        }
       }
       // calculate final confidence
       float confidence = box_confidence * class_score;
@@ -333,7 +326,7 @@ class YoloV5 {
   }
 #endif
 
-  std::vector<Detection> postprocess(const cv::Mat &image,
+  std::vector<Detection> postprocess(const cv::Mat& image,
                                      std::vector<DetectOutput> outputs,
                                      bool enable_ort) {
     int ret = -1;
@@ -347,7 +340,7 @@ class YoloV5 {
       ret = infer_detections(model_path, outputs, detections);
 #endif
       ;
-    } else{
+    } else {
       ret = calculate_detections(outputs, detections);
     }
 
@@ -357,18 +350,17 @@ class YoloV5 {
     return detections;
   }
 
-private:
+ private:
   int min_wh_{0};
   int max_wh_{7680};
   float iou_threshold_{0.45f};
   float conf_threshold_{0.25f};
-  const int input_sizes_[2] = {640, 640}; // wh
+  const int input_sizes_[2] = {640, 640};  // wh
   const int num_anchors_{25200};
   const int num_classes_{80};
   int img_rows_{0};
   int img_cols_{0};
 };
-
 
 int main(int argc, char* argv[]) {
   printf("\n===> yolov5s c++ example start...\n");
@@ -380,13 +372,16 @@ int main(int argc, char* argv[]) {
     enable_ort = true;
   }
 #endif
-  printf("tcim version: %s, enable ort: %d.\n", tcim::GetVersion().c_str(), enable_ort);
+  printf("tcim version: %s, enable ort: %d.\n", tcim::GetVersion().c_str(),
+         enable_ort);
 
   // 1. load model
   std::cout << "LoadFromFile yolov5s" << std::endl;
   std::string model_path = "yolov5s.hmm";
   if (!fs::exists(model_path)) {
-    std::cerr << model_path << " not exist. you should run build.py in yolov5s example first." << std::endl;
+    std::cerr << model_path
+              << " not exist. you should run build.py in yolov5s example first."
+              << std::endl;
     exit(-1);
   }
   auto module = tcim::Module::LoadFromFile(model_path);
@@ -405,7 +400,8 @@ int main(int argc, char* argv[]) {
     auto input_info = module.GetInputInfo(input_name).AsContiguous();
     std::cout << "Input[" << input_name << "] " << input_info << std::endl;
     auto input_tensor = tcim::Tensor::CreateHostTensor(input_info);
-    input_map.insert(std::pair<std::string, tcim::Tensor>(input_name, input_tensor));
+    input_map.insert(
+        std::pair<std::string, tcim::Tensor>(input_name, input_tensor));
   }
 
   // 3. input preprocess
@@ -422,11 +418,12 @@ int main(int argc, char* argv[]) {
   img_rgb = letterbox(img_raw, 640, 640);
   // cv::resize(img_raw, img_raw, cv::Size(1920, 1080));
   // img_rgb = img_raw.clone();
-  ImageProc::BgrToRgb((int8_t *)(img_rgb.data), img_rgb.rows, img_rgb.cols);
+  ImageProc::BgrToRgb((int8_t*)(img_rgb.data), img_rgb.rows, img_rgb.cols);
   cv::cvtColor(img_rgb, img_yuv, cv::COLOR_RGB2YUV_I420);
   int size = 640 * 640 * 3;
   // int size = 1080 * 1920 * 3;
-  ImageProc::I420To420sp((uint8_t *)input_map.at("images").Data(), (uint8_t *)img_yuv.data, size);
+  ImageProc::I420To420sp((uint8_t*)input_map.at("images").Data(),
+                         (uint8_t*)img_yuv.data, size);
   auto it = input_map.find("dyn_info");
   if (it != input_map.end()) {
     int32_t dyn_info[10] = {0, 0, 1080, 1920, 360, 640, 140, 0, 140, 0};
@@ -439,10 +436,12 @@ int main(int argc, char* argv[]) {
   std::cout << "Count of Output: " << output_num << std::endl;
   for (int idx = 0; idx < output_num; idx++) {
     auto output_name = module.GetOutputName(idx);
-    auto output_info = module.GetOutputInfo(output_name).AsContiguous().AsType(tcim::FLOAT32);
+    auto output_info =
+        module.GetOutputInfo(output_name).AsContiguous().AsType(tcim::FLOAT32);
     std::cout << "Output[" << output_name << "] " << output_info << std::endl;
     auto output_tensor = tcim::Tensor::CreateHostTensor(output_info);
-    output_map.insert(std::pair<std::string, tcim::Tensor>(output_name, output_tensor));
+    output_map.insert(
+        std::pair<std::string, tcim::Tensor>(output_name, output_tensor));
   }
 
   // 5. set input
@@ -477,10 +476,12 @@ int main(int argc, char* argv[]) {
   // 9. print and draw
   printf("detect num: %d\n", (int)detections.size());
   for (const auto& detection : detections) {
-    printf("box[%d, %d, %d, %d], conf:%f, cls:%d\n",
-           detection.box.x1, detection.box.y1, detection.box.x2, detection.box.y2, detection.conf, detection.cls);
+    printf("box[%d, %d, %d, %d], conf:%f, cls:%d\n", detection.box.x1,
+           detection.box.y1, detection.box.x2, detection.box.y2, detection.conf,
+           detection.cls);
     cv::rectangle(img_raw, cv::Point(detection.box.x1, detection.box.y1),
-                  cv::Point(detection.box.x2, detection.box.y2), cv::Scalar(0, 0, 255), 2);
+                  cv::Point(detection.box.x2, detection.box.y2),
+                  cv::Scalar(0, 0, 255), 2);
   }
   fs::path file_path(data_path);
   fs::path result_path("demo_results/cpp");
@@ -496,7 +497,7 @@ int main(int argc, char* argv[]) {
     std::cout << "detect num != 18" << std::endl;
     exit(-1);
   }
-  
+
   printf("<=== yolov5s c++ example completed.\n\n");
   return 0;
 }
