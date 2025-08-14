00bbef19e5b3e845c39708a12dcad44056c0565a
wanyu.li <wanyu.li@houmo.ai>
2025-08-14 15:05:58 +0800

    HMSW-2253: resnet50 cpp example supports xh2
    Change-Id: I66cb097f819b43ac3d4306ffd5fbcb357848459a
    (cherry picked from commit 083bec34d00dd344c7ebbf48b5ebff331dcba72e)

--- apis/inferences/resnet50/resnet50.cc
+++ apis/inferences/resnet50/resnet50.cc
@@ -1,9 +1,9 @@
 // Copyright (c) 2022 The Houmo.ai Authors. All rights reserved.
 
+#include <cstdlib>
 #include <iostream>
 #include <sstream>
 #include <string>
-#include <cstdlib>
 
 #if (__GNUC__ < 8 && !defined(_MSC_VER))
 #include <experimental/filesystem>
@@ -17,49 +17,47 @@ namespace fs = std::filesystem;
 #include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
 
-#include "tcim/tcim_runtime.h"
-
 #include "datasets/imagenet.hpp"
 #include "imageproc.hpp"
+#include "tcim/tcim_runtime.h"
 #include "threads.hpp"
 #include "utils.hpp"
-
 
 template <typename T>
 int get_topk(int topk, std::vector<std::pair<T, int>> sort_pairs) {
   std::sort(sort_pairs.begin(), sort_pairs.end(),
             [](const std::pair<T, int>& a, const std::pair<T, int>& b) {
-            return a.first > b.first;
+              return a.first > b.first;
             });
 
   for (int i = 0; i < topk; ++i) {
-    std::cout << "top" << (i + 1) << ": Index="
-              << sort_pairs[i].second << " Conf=" << sort_pairs[i].first
-              << ", Label=[" << Imagenet::GetLabel(sort_pairs[i].second) << "]" << std::endl;
+    std::cout << "top" << (i + 1) << ": Index=" << sort_pairs[i].second
+              << " Conf=" << sort_pairs[i].first << ", Label=["
+              << Imagenet::GetLabel(sort_pairs[i].second) << "]" << std::endl;
   }
 
   return sort_pairs[0].second;
 }
 
-
 int main() {
   printf("\n===> resnet50 c++ example start...\n");
   const char* houmo_target_env = getenv("HOUMO_TARGET");
-  std::string houmo_target = houmo_target_env != nullptr ? std::string(houmo_target_env) : "houmo";
-  if (houmo_target != "xh1") {
-    std::cerr << "Not support houmo target:" << houmo_target << std::endl;
-    exit(-1);
-  }
-  printf("tcim version: %s.\n", tcim::GetVersion().c_str());
+  std::string houmo_target =
+      houmo_target_env != nullptr ? std::string(houmo_target_env) : "houmo";
+  printf("tcim version: %s, houmo_target:%s.\n", tcim::GetVersion().c_str(),
+         houmo_target.c_str());
 
   // 1. load model
   std::cout << "LoadFromFile resnet50" << std::endl;
   std::string model_path = "./resnet50.hmm";
   if (houmo_target == "xh2") {
-    model_path =  "./resnet50_xh2_b1_1core.hmm";
+    model_path = "./resnet50_xh2_b1_1core.hmm";
   }
   if (!fs::exists(model_path)) {
-    std::cerr << model_path << " not exist. you should run build.py in resnet50 example first." << std::endl;
+    std::cerr
+        << model_path
+        << " not exist. you should run build.py in resnet50 example first."
+        << std::endl;
     exit(-1);
   }
   auto module = tcim::Module::LoadFromFile(model_path);
@@ -80,10 +78,13 @@ int main() {
     auto input_info_f32 = input_info.AsType(tcim::DataType::FLOAT32);
     std::cout << "Input[" << input_name << "] " << input_info << std::endl;
     auto input_tensor = tcim::Tensor::CreateHostTensor(input_info);
-    input_map.insert(std::pair<std::string, tcim::Tensor>(input_name, input_tensor));
+    input_map.insert(
+        std::pair<std::string, tcim::Tensor>(input_name, input_tensor));
     if (houmo_target == "xh2") {
-      std::cout << "Input f32[" << input_name << "] " << input_info_f32 << std::endl;
-      input_map_f32.insert(std::pair<std::string, tcim::TensorInfo>(input_name, input_info_f32));
+      std::cout << "Input f32[" << input_name << "] " << input_info_f32
+                << std::endl;
+      input_map_f32.insert(
+          std::pair<std::string, tcim::TensorInfo>(input_name, input_info_f32));
     }
   }
 
@@ -97,11 +98,12 @@ int main() {
   img_rgb = cv::imread(data_path);
   if (houmo_target == "xh1") {
     cv::Mat img_yuv;
-    ImageProc::BgrToRgb((int8_t *)(img_rgb.data), img_rgb.rows, img_rgb.cols);
+    ImageProc::BgrToRgb((int8_t*)(img_rgb.data), img_rgb.rows, img_rgb.cols);
     cv::resize(img_rgb, img_rgb, {224, 224});
     cv::cvtColor(img_rgb, img_yuv, cv::COLOR_RGB2YUV_I420);
-    int size = 224*224*3;
-    ImageProc::I420To420sp((uint8_t *)input_map.at("input.1").Data(), (uint8_t *)img_yuv.data, size);
+    int size = 224 * 224 * 3;
+    ImageProc::I420To420sp((uint8_t*)input_map.at("input.1").Data(),
+                           (uint8_t*)img_yuv.data, size);
   } else if (houmo_target == "xh2") {
     cv::Mat img_norm;
     const float mean[3] = {123.675f, 116.28f, 103.53f};
@@ -110,16 +112,22 @@ int main() {
     cv::resize(img_rgb, img_rgb, {224, 224});
 
     img_rgb.convertTo(img_norm, CV_32FC3);
-    cv::Mat channels[3];
+    std::vector<cv::Mat> channels;
     cv::split(img_norm, channels);
     for (int i = 0; i < 3; ++i) {
-        channels[i] = (channels[i] - mean[i]) / std[i];
+      channels[i] = (channels[i] - mean[i]) / std[i];
     }
-    cv::merge(channels, 3, img_norm);
+    // HWC --> CHW
+    for (auto& ch : channels) {
+      ch = ch.reshape(1, 1);
+    }
+    cv::vconcat(channels, img_norm);
 
     size_t img_bytes = img_norm.total() * img_norm.elemSize();
-    auto input_tensor_f32 = tcim::Tensor::CreateHostTensor(
-      input_map_f32.at("input.1"), img_bytes, static_cast<void*>(img_norm.data));
+    std::cout << "img_bytes: " << img_bytes << std::endl;
+    auto input_tensor_f32 =
+        tcim::Tensor::CreateHostTensor(input_map_f32.at("input.1"), img_bytes,
+                                       reinterpret_cast<void*>(img_norm.data));
     input_tensor_f32.CastTo(input_map.at("input.1"));
   }
 
@@ -133,11 +141,13 @@ int main() {
     auto output_info = module.GetOutputInfo(output_name).AsContiguous();
     std::cout << "Output[" << output_name << "] " << output_info << std::endl;
     auto output_tensor = tcim::Tensor::CreateHostTensor(output_info);
-    output_map.insert(std::pair<std::string, tcim::Tensor>(output_name, output_tensor));
+    output_map.insert(
+        std::pair<std::string, tcim::Tensor>(output_name, output_tensor));
     if (houmo_target == "xh2") {
       auto output_info_f32 = output_info.AsType(tcim::DataType::FLOAT32);
       auto output_tensor_f32 = tcim::Tensor::CreateHostTensor(output_info_f32);
-      output_map_f32.insert(std::pair<std::string, tcim::Tensor>(output_name, output_tensor_f32));
+      output_map_f32.insert(
+          std::pair<std::string, tcim::Tensor>(output_name, output_tensor_f32));
     }
   }
 
