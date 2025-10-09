f312a3025ed1e9bf4da98b95765056090d3dbd38
wanyu.li <wanyu.li@houmo.com>
2025-10-09 10:19:40 +0800

    HMSW-2603: update hmm model
    Change-Id: I873ca4961bdda574eadc568d9cc29667c16f4c81

--- models/llm/deepseek/get_model.py
+++ models/llm/deepseek/get_model.py
@@ -1,6 +1,6 @@
 import os
-import onnx
 import argparse
+
 from hmatc.utils.utils import get_file_from_jfrog
 
 
@@ -19,10 +19,16 @@ def get_args() -> argparse.Namespace:
         help='which resource to get, choise in [raw, hmm]',
     )
     parser.add_argument(
+        "--build_model_dir",
+        dest="build_model_dir",
+        type=str,
+        default=os.path.join("output", HOUMO_TARGET),
+    )
+    parser.add_argument(
         '--model_dir',
         dest='model_dir',
         type=str,
-        default='',
+        default='.',
         help='where to save downloaded model',
     )
     args = parser.parse_args()
@@ -31,13 +37,13 @@ def get_args() -> argparse.Namespace:
 
 if __name__ == '__main__':
     args = get_args()
-    quant_model_dir = args.quant_model_dir
+    build_model_dir = args.build_model_dir
     model_type = args.model_type
     model_dir = args.model_dir
     HOUMO_DATASETS_PATH = os.getenv('HOUMO_DATASETS_PATH', '.')
     HOUMO_MODEL_PATH = os.getenv('HOUMO_MODEL_PATH', '.')
     wiki_path = "models/datasets/wikitext-2-raw-v1.zip"
-    hmm_path = "models/deepseek/hmm_deepseek_256_8192_4cores_20250322.zip"
+    hmm_path = "models/deepseek/hmm_deepseek_256_8192_4cores_20250922.zip"
 
     if model_type in ["raw"]:
         ignore_patterns = []
@@ -46,9 +52,12 @@ if __name__ == '__main__':
         ignore_patterns = ["*.safetensors"]
 
     from modelscope import snapshot_download
-    snapshot_download('deepseek-ai/DeepSeek-R1-Distill-Qwen-7B',
-                      local_dir='DeepSeek-R1-Distill-Qwen-7B',
-                      ignore_patterns=ignore_patterns)
+
+    snapshot_download(
+        'deepseek-ai/DeepSeek-R1-Distill-Qwen-7B',
+        local_dir=f"{model_dir}/DeepSeek-R1-Distill-Qwen-7B",
+        ignore_patterns=ignore_patterns,
+    )
 
     if model_type in ["hmm"]:
-        get_file_from_jfrog(hmm_path, model_dir, os.path.join('output', HOUMO_TARGET))
+        get_file_from_jfrog(hmm_path, model_dir, build_model_dir)
