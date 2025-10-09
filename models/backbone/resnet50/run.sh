47ef4eb588b4937126efb6df4e51199ea92329e7
weiguo.xing <weiguo.xing@houmo.ai>
2025-10-09 01:48:21 +0000

    HMSW-2606: Remove run.sh
    Change-Id: Ie34c6ef0d8418e9a313d335863dabe1774bc6115

--- models/backbone/resnet50/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 执行 100 次你的命令
-for i in {1..100}
-do
-    echo "执行第 $i 次"
-    # hmatc perf -c config.yml -wn 10 -sn 100 -tn 4
-    hmatc perf -m xh2_conv.hmm -wn 10 -sn 100 -tn 2
-done
