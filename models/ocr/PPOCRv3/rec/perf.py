3af643c00bf59fa14cf09725b63e3e9788eba55f
nan.xu <nan.xu@houmo.ai>
2025-10-14 17:17:20 +0800

    HMSW-2474: fix ppocrv3 rec xh2 perf error
    Change-Id: I0df4591e1ba3e79fb93db10d2ffa5d30dbf7bcfd

--- models/ocr/PPOCRv3/rec/perf.py
+++ models/ocr/PPOCRv3/rec/perf.py
@@ -7,7 +7,7 @@ model_path = "./output/xh1/ocr_rec.hmm"
 warmup_num = 1
 sample_num = 1
 loop_num = 1
-device_num = 1
+device_id = 0
 thread_num = 1
 if len(sys.argv) > 1:
     logger.info(f"perf -> hmm path: {sys.argv[1]}")
@@ -22,8 +22,8 @@ elif len(sys.argv) > 4:
     logger.info(f"perf -> loop_num = {sys.argv[4]}")
     loop_num = int(sys.argv[4])
 elif len(sys.argv) > 5:
-    logger.info(f"perf -> device_num = {sys.argv[5]}")
-    device_num = int(sys.argv[5])
+    logger.info(f"perf -> device_id = {sys.argv[5]}")
+    device_id = int(sys.argv[5])
 elif len(sys.argv) > 6:
     logger.info(f"perf -> thread_num = {sys.argv[6]}")
     thread_num = int(sys.argv[6])
@@ -32,4 +32,4 @@ logger.info(f"Using model: {model_path}")
 if not os.path.exists(model_path):
     raise FileNotFoundError(f"Model file not found: {model_path}")
 
-perf_info = perf.CModelRunner(model_path, sample_num, thread_num, device_num, loop_num, warmup_num)
+perf_info = perf.CModelRunner(model_path, sample_num, thread_num, device_id, loop_num, warmup_num)
