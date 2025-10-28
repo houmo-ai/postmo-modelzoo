ed286298816555faebe2d263c8be6114af4b5d04
weiguo.xing <weiguo.xing@houmo.ai>
2025-10-28 07:44:41 +0000

    HMSW-2746: Fixed bandwidth and computing on ubuntu20.04/22.04
    Change-Id: I9afa6f7c3bc3de984ff4333d45af8eb2067d51df

--- apis/tools/computing_perf/run.sh
+++ apis/tools/computing_perf/run.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+WORK_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${WORK_PATH}" || exit 1
+
+if [ "${HDPL_PLATFORM:-}" = "ISIM" ]; then
+    echo "ISIM platform does not support this tool."
+    exit 0
+fi
+
 python3 computing_perf.py
