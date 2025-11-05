2397deac544a15f41e10905a400c95015592cc2c
wanyu.li <wanyu.li@houmo.com>
2025-11-05 11:40:45 +0800

    HMSW-2803: fix import error
    Change-Id: I131c6805b1dcdf47c3a8c9e127b2a68e281bc4c6

--- apis/inferences/qwen3/get_model.py
+++ apis/inferences/qwen3/get_model.py
@@ -1,8 +1,11 @@
 import os
+import sys
 import argparse
-from hmatc.utils.utils import get_file_from_jfrog
 
-HOUMO_EXAMPLES_PATH = os.environ.get("HOUMO_EXAMPLES_PATH", "..")
+HOUMO_EXAMPLES_PATH = os.environ.get('HOUMO_EXAMPLES_PATH', '../../..')
+sys.path.append(f'{HOUMO_EXAMPLES_PATH}/hmatc')
+from hmatc.utils.utils import get_file_from_jfrog, get_houmo_version
+
 HOUMO_TARGET = os.getenv("HOUMO_TARGET")
 assert HOUMO_TARGET in ["xh1", "xh2"], f"Unsupported HOUMO_TARGET: {HOUMO_TARGET}"
 
@@ -40,13 +43,16 @@ if __name__ == '__main__':
         else args.model_dir
     )
 
-    if HOUMO_TARGET == "xh1":
-        if args.ncore == 2:
-            hmm_path = "models/qwen3/hmm_qwen3_256_8k_2cores_20250603.zip"
-        elif args.ncore == 4:
-            hmm_path = "models/qwen3/hmm_qwen3_256_8k_4cores_20250728.zip"
-    elif HOUMO_TARGET == "xh2":
-        hmm_path = "models/qwen3/hmm_xh2_qwen3_8b_256_8k_2cores_20250808.zip"
+    model_name = "qwen3"
+    model_size = "8b"
+    ncore = "2cores" if HOUMO_TARGET == "xh2" else f"{args.ncore}cores"
+    ndevice = "1chip"
+    context_len = "8k"
+    prefill_len = 256
+    batch = 1
+    version = get_houmo_version()
+    target = HOUMO_TARGET
+    hmm_path = f"models/{target.lower()}-{version}/{model_name}/hmm_{target}_{model_name}_{model_size}_{prefill_len}_{context_len}_b{batch}_{ndevice}_{ncore}_{version}.zip"
 
     from modelscope import snapshot_download
 
@@ -54,20 +60,24 @@ if __name__ == '__main__':
         "qwen/qwen3-8b", local_dir="qwen3-8b", ignore_patterns=["*.safetensors"]
     )
 
-    get_file_from_jfrog(hmm_path, model_dir, "./")
+    get_file_from_jfrog(hmm_path, model_dir, ".")
+
     embedding_path = "hmquant/quant_embedding.pt"
-    if os.path.exists(embedding_path) and HOUMO_TARGET in ["xh1", "xh2"]:
+    if os.path.exists(embedding_path):
         print(HOUMO_TARGET)
         import torch
         import numpy as np
 
-        embedding_weight = torch.load(embedding_path, map_location="cpu", weights_only=True)
+        embedding_weight = torch.load(
+            embedding_path, map_location="cpu", weights_only=True
+        )
         if HOUMO_TARGET == "xh2":
             embedding_weight = embedding_weight['weight']
         if embedding_weight.dtype == torch.bfloat16:
             embedding_weight = embedding_weight.float().half()
-        embedding_data = embedding_weight.cpu().numpy() 
+        embedding_data = embedding_weight.cpu().numpy()
         embedding_data.tofile(embedding_path.replace(".pt", ".bin"))
+
     # 下载编译好的三方库
     thirdparty_path = "models/qwen3/3rdparty.zip"
     get_file_from_jfrog(thirdparty_path, model_dir, "./")
