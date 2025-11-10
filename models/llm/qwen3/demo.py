ff304a7bc88c021aa83913a3f45939ca94f0d121
xinyu.ge <xinyu.ge@houmo.ai>
2025-11-10 10:46:07 +0800

    HMSW-2833: demo merge xh1&&xh2 class
    Signed-off-by: xinyu.ge <xinyu.ge@houmo.ai>
    Change-Id: I52c37374ce7dfdce84a8cf159b94c331a3fab207

--- models/llm/qwen3/demo.py
+++ models/llm/qwen3/demo.py
@@ -6,8 +6,7 @@ import sys
 import math
 import time
 import argparse
-from typing import List, Optional
-
+from typing import List, Tuple, Optional
 import numpy as np
 import torch
 import torch.nn.functional as F
@@ -235,7 +234,9 @@ class SamplingManager:
     def sample(
         self, logits: np.ndarray, previous_tokens: Optional[List[int]] = None
     ) -> int:
-        logits = logits[0][0]
+        logits = logits[0]
+        if HOUMO_TARGET == "xh2":
+            logits = logits[0]
         probs = self.process_logits(logits, previous_tokens)
         if np.all(probs == 0):
             probs = np.ones_like(probs) / len(probs)
@@ -243,7 +244,7 @@ class SamplingManager:
         # sampled_index = np.random.choice(len(probs), p=probs)
         sampled_index = probs.argmax(-1)
 
-        return np.array([sampled_index])
+        return np.array([[sampled_index]])
 
     def get_processed_probs(
         self, logits: np.ndarray, previous_tokens: Optional[List[int]] = None
@@ -253,196 +254,13 @@ class SamplingManager:
 
 class HmQwen:
 
-    def __init__(self, prefill_path, decode_path, embedding_path, tokenizer_dir):
-        weight_manager = tcim.runtime.WeightManager(0)
-        option1 = tcim.runtime.Option(weight_manager)
-        option2 = tcim.runtime.Option(weight_manager)
-        self.prefill = tcim.runtime.load(prefill_path, option=option1)
-        logger.info("prefill model loaded")
-        self.nblocks = self.get_nblocks()
-        dummy_tensor_names = [
-            f"model_layers_{i}_self_attn_kcache_input" for i in range(self.nblocks)
-        ]
-        dummy_tensor_names += [
-            f"model_layers_{i}_self_attn_vcache_input" for i in range(self.nblocks)
-        ]
-        option2.set_dummy_tensors(dummy_tensor_names)
-        self.decode = tcim.runtime.load(decode_path, option=option2)
-        logger.info("decode model loaded")
-        prefill_input_shape = self.prefill.get_input_info(
-            self.prefill.get_input_name(0)
-        ).shape
-        self.prefill_length = prefill_input_shape[1] * prefill_input_shape[0]
-        self.embedding_len = self.prefill.get_input_info(
-            self.prefill.get_input_name(0)
-        ).shape[2]
-        self.context_max_length = self.decode.get_input_info(
-            self.decode.get_input_name(3)
-        ).shape[2]
-        self.batch = self.decode.get_input_info(self.decode.get_input_name(0)).shape[0]
-        # set kvcache input
-        for i in range(self.nblocks):
-            kcache = self.prefill.get_input(f"model_layers_{i}_self_attn_kcache_input")
-            self.decode.set_input(f"model_layers_{i}_self_attn_kcache_input", kcache)
-            vcache = self.prefill.get_input(f"model_layers_{i}_self_attn_vcache_input")
-            self.decode.set_input(f"model_layers_{i}_self_attn_vcache_input", vcache)
-        # set decode input
-        current_length_input_1 = np.array([1]).astype("int16")
-        self.decode.set_input("current_length", current_length_input_1)
-
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            tokenizer_dir, trust_remote_code=True
-        )
-        embedding_weight = torch.load(embedding_path, map_location="cpu")
-        self.embedding_weight = embedding_weight.reshape(-1, self.embedding_len)
-
-    def get_nblocks(self):
-        input_names = []
-        for i in range(self.prefill.get_num_inputs()):
-            input_names.append(self.prefill.get_input_name(i))
-        pattern = r"^model_layers_(\d+)_self_attn_kcache_input$"
-        count = sum(1 for item in input_names if re.match(pattern, item))
-        return count
-
-    def chat(self, question):
-        logger.success("question:")
-        print("\033[1;95m{}\033[0m".format(question))
-        start_time = time.time()
-        messages = [
-            {
-                "role": "system",
-                "content": "Please reason step by step, and put your final answer within \\boxed{}.",
-            },
-            {
-                "role": "user",
-                "content": question,
-            },
-        ]
-        text = self.tokenizer.apply_chat_template(
-            messages, tokenize=False, add_generation_prompt=True
-        )
-        inputs = self.tokenizer(text, return_tensors="pt", add_special_tokens=False)
-        text = self.tokenizer.batch_decode(inputs.input_ids)[0]
-        all_input_ids = inputs["input_ids"]
-        input_echo_len = all_input_ids.numel()
-        if input_echo_len >= self.context_max_length:
-            logger.error(
-                f"Question long than {self.context_max_length}, please shorten it!"
-            )
-            sys.exit(1)
-
-        prefill_loop_round = math.ceil(input_echo_len / self.prefill_length)
-        for round in range(prefill_loop_round):
-            valid_length = round * self.prefill_length
-            if round == prefill_loop_round - 1:
-                current_length = input_echo_len - round * self.prefill_length
-                input_ids = all_input_ids[
-                    :, round * self.prefill_length : input_echo_len
-                ]
-            else:
-                current_length = self.prefill_length
-                input_ids = all_input_ids[
-                    :, round * self.prefill_length : (round + 1) * self.prefill_length
-                ]
-            inputs_embeds = F.embedding(input_ids, self.embedding_weight)
-            effective_length = input_ids.size(-1)
-            _pad_embeds = torch.zeros(
-                1,
-                self.prefill_length - effective_length,
-                inputs_embeds.size(-1),
-                dtype=inputs_embeds.dtype,
-                device=inputs_embeds.device,
-            )
-
-            input_data = torch.cat([inputs_embeds, _pad_embeds], dim=1).reshape(
-                4, self.prefill_length // 4, self.embedding_len
-            )
-            valid_length_data = np.array([valid_length]).astype("int16")
-            current_length_data = np.array([current_length]).astype("int16")
-            self.prefill.set_input("input_1", input_data.numpy())
-            self.prefill.set_input("valid_length", valid_length_data)
-            self.prefill.set_input("current_length", current_length_data)
-            self.prefill.run()
-            self.prefill.sync()
-
-        input_data = self.prefill.get_output("Output_lm_head_add_list_0").numpy()
-        next_id = input_data.argmax(-1)
-        prefill_response = self.tokenizer.decode(next_id.tolist())
-        prefill_time = time.time() - start_time
-        chat_history_ids = all_input_ids[0]
-        next_id = torch.from_numpy(next_id)
-
-        chat_history_ids = torch.cat([chat_history_ids, next_id], dim=-1)
-        input_data = F.embedding(next_id.unsqueeze(0), self.embedding_weight).reshape(
-            1, 1, -1
-        )
-        all_response = prefill_response
-        context_length = input_echo_len
-        logger.success("response:")
-        print("\033[1;95m{}".format(prefill_response), end="", flush=True)
-
-        decode_count = 0
-        skip_tokens = 0
-        slide_len = 10  # sliding window length for decode
-        last_response = self.tokenizer.decode(chat_history_ids.tolist()[-slide_len:])
-
-        start_time = time.time()
-        while True:
-            if context_length >= self.context_max_length:
-                logger.info(
-                    f"context length greater than {self.context_max_length}, break!"
-                )
-                break
-
-            self.decode.set_input("input_1", input_data.numpy())
-            valid_length_data = np.array(context_length).astype("int16")
-            self.decode.set_input("valid_length", valid_length_data)
-            self.decode.run()
-            self.decode.sync()
-            input_data = self.decode.get_output("Output_lm_head_add_list_0").numpy()
-            decode_count += 1
-
-            next_id = input_data.argmax(-1)
-            next_id = torch.from_numpy(next_id)
-            if next_id == self.tokenizer.eos_token_id:
-                print(decode_response, end="", flush=True)
-                all_response += decode_response
-                break
-
-            chat_history_ids = torch.cat([chat_history_ids, next_id], dim=-1)
-            decode_response = self.tokenizer.decode(
-                chat_history_ids.tolist()[-(slide_len + 1) - skip_tokens :]
-            )[len(last_response) :]
-            if decode_response != "" and is_valid_char(ord(decode_response[-1])):
-                print(decode_response, end="", flush=True)
-                all_response += decode_response
-                last_response = self.tokenizer.decode(
-                    chat_history_ids.tolist()[-slide_len:]
-                )
-                skip_tokens = 0
-            else:
-                skip_tokens += 1
-
-            input_data = F.embedding(
-                next_id.unsqueeze(0), self.embedding_weight
-            ).reshape(1, 1, -1)
-            context_length = context_length + 1
-
-        decode_time = time.time() - start_time
-        print("\033[0m")
-
-        return all_response, input_echo_len, decode_count + 1, prefill_time, decode_time
-
-
-class HmQwenXh2:
-
     def __init__(
-        self, prefill_path, decode_path, embedding_path, tokenizer_dir, ndevice
+        self, prefill_path, decode_path, embedding_path, tokenizer_dir, ndevice=1
     ):
         self.ndevice = ndevice
         if self.ndevice == 1:
             weight_manager = tcim.runtime.WeightManager(0)
-        elif self.ndevice == 2:
+        elif self.ndevice == 2 and HOUMO_TARGET == "xh2":
             dev_manager = tcim.runtime.DevManager([0, 1], "Xh2HalBackend")
             weight_manager = tcim.runtime.WeightManager(dev_manager)
         else:
@@ -467,19 +285,52 @@ class HmQwenXh2:
             top_p=args.topp,
             repetition_penalty=args.repetition_penalty,
         )
-        self.prefill_length = self.prefill.get_input_info(
-            self.prefill.get_input_name(0)
-        ).shape[1]
-        self.embedding_len = self.prefill.get_input_info(
-            self.prefill.get_input_name(0)
-        ).shape[2]
-        self.context_max_length = self.decode.get_input_info(
-            self.decode.get_input_name(3)
-        ).shape[2]
-        self.batch = self.decode.get_input_info(self.decode.get_input_name(0)).shape[0]
-        for i in range(3, 2 * self.nblocks + 3):
-            cache = self.prefill.get_input(self.prefill.get_input_name(i))
-            self.decode.set_input(self.decode.get_input_name(i), cache)
+        if HOUMO_TARGET == "xh1":
+            prefill_input_shape = self.prefill.get_input_info(
+                self.prefill.get_input_name(0)
+            ).shape
+            self.prefill_length = prefill_input_shape[1] * prefill_input_shape[0]
+            self.embedding_len = self.prefill.get_input_info(
+                self.prefill.get_input_name(0)
+            ).shape[2]
+            self.context_max_length = self.decode.get_input_info(
+                self.decode.get_input_name(3)
+            ).shape[2]
+            self.batch = self.decode.get_input_info(
+                self.decode.get_input_name(0)
+            ).shape[0]
+        elif HOUMO_TARGET == "xh2":
+            self.prefill_length = self.prefill.get_input_info(
+                self.prefill.get_input_name(0)
+            ).shape[1]
+            self.embedding_len = self.prefill.get_input_info(
+                self.prefill.get_input_name(0)
+            ).shape[2]
+            self.context_max_length = self.decode.get_input_info(
+                self.decode.get_input_name(3)
+            ).shape[2]
+            self.batch = self.decode.get_input_info(
+                self.decode.get_input_name(0)
+            ).shape[0]
+
+        if HOUMO_TARGET == "xh1":
+            for i in range(self.nblocks):
+                kcache = self.prefill.get_dev_input(
+                    f"model_layers_{i}_self_attn_kcache_input"
+                )
+                self.decode.set_input(
+                    f"model_layers_{i}_self_attn_kcache_input", kcache
+                )
+                vcache = self.prefill.get_dev_input(
+                    f"model_layers_{i}_self_attn_vcache_input"
+                )
+                self.decode.set_input(
+                    f"model_layers_{i}_self_attn_vcache_input", vcache
+                )
+        elif HOUMO_TARGET == "xh2":
+            for i in range(3, 2 * self.nblocks + 3):
+                cache = self.prefill.get_input(self.prefill.get_input_name(i))
+                self.decode.set_input(self.decode.get_input_name(i), cache)
         # set decode input
         current_length_input_1 = np.array([1]).astype("int32")
         decode_current_length_name = self.decode.get_input_name(2)
@@ -488,9 +339,9 @@ class HmQwenXh2:
         self.tokenizer = AutoTokenizer.from_pretrained(
             tokenizer_dir, trust_remote_code=True
         )
-        embedding_weight = torch.load(
-            embedding_path, map_location="cpu", weights_only=True
-        )["weight"]
+        embedding_weight = torch.load(embedding_path, map_location="cpu")
+        if HOUMO_TARGET == "xh2":
+            embedding_weight = embedding_weight["weight"]
         self.embedding_weight = embedding_weight.reshape(-1, self.embedding_len)
         self.generated_ids = []
 
@@ -548,12 +399,18 @@ class HmQwenXh2:
                 dtype=inputs_embeds.dtype,
                 device=inputs_embeds.device,
             )
-            # [256, 1, 4096] ==> [4, 64, 4096]
-            input_data = torch.cat([inputs_embeds, _pad_embeds], dim=1).reshape(
-                1, self.prefill_length, self.embedding_len
-            )
-            valid_length_data = np.array([valid_length]).astype("int32")
-            current_length_data = np.array([current_length]).astype("int32")
+            if HOUMO_TARGET == "xh1":
+                input_data = torch.cat([inputs_embeds, _pad_embeds], dim=1).reshape(
+                    4, self.prefill_length // 4, self.embedding_len
+                )
+                valid_length_data = np.array([valid_length]).astype("int16")
+                current_length_data = np.array([current_length]).astype("int16")
+            elif HOUMO_TARGET == "xh2":
+                input_data = torch.cat([inputs_embeds, _pad_embeds], dim=1).reshape(
+                    1, self.prefill_length, self.embedding_len
+                )
+                valid_length_data = np.array([valid_length]).astype("int32")
+                current_length_data = np.array([current_length]).astype("int32")
             input_name = self.prefill.get_input_name(0)
             valid_length_name = self.prefill.get_input_name(1)
             current_length_name = self.prefill.get_input_name(2)
@@ -565,6 +422,8 @@ class HmQwenXh2:
 
         input_data = self.prefill.get_output(self.prefill.get_output_name(0)).numpy()
         next_id = input_data.argmax(-1)[0]
+        if HOUMO_TARGET == "xh1":
+            next_id = np.array([next_id])
         prefill_response = self.tokenizer.decode(next_id)
         prefill_time = time.time() - start_time
         chat_history_ids = all_input_ids[0]
@@ -586,6 +445,7 @@ class HmQwenXh2:
         last_response = self.tokenizer.decode(chat_history_ids.tolist()[-slide_len:])
         decode_time = 0
 
+        start_time = time.time()
         while True:
             if context_length >= self.context_max_length:
                 logger.info(
@@ -604,8 +464,11 @@ class HmQwenXh2:
             decode_time += time.time() - start_time
             input_data = self.decode.get_output(self.decode.get_output_name(0)).numpy()
             decode_count += 1
+
             next_id = self.samplingmanager.sample(input_data, self.generated_ids)
-            next_id = torch.from_numpy(next_id)
+            if HOUMO_TARGET == "xh1":
+                next_id = np.array(next_id)
+            next_id = torch.from_numpy(next_id[0])
             if next_id == self.tokenizer.eos_token_id:
                 print(decode_response, end="", flush=True)
                 all_response += decode_response
@@ -641,10 +504,13 @@ if __name__ == "__main__":
     args = get_args()
     if HOUMO_TARGET == "xh1":
         hmqwen = HmQwen(
-            args.prefill_path, args.decode_path, args.embedding_path, args.tokenizer_dir
+            args.prefill_path,
+            args.decode_path,
+            args.embedding_path,
+            args.tokenizer_dir,
         )
     elif HOUMO_TARGET == "xh2":
-        hmqwen = HmQwenXh2(
+        hmqwen = HmQwen(
             args.prefill_path,
             args.decode_path,
             args.embedding_path,
