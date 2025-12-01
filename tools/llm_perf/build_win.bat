6d4d5aeab07fd36843131792ad85ce0a1b66e6aa
yaozhen.liu <yaozhen.liu@houmo.ai>
2025-12-01 10:02:36 +0800

    HMSW-2987 : add auto download and unzip 3rdparty.zip to build_win.bat.
    Change-Id: I6927f1b6d973018c3e58a8ea31452e081643e0dd

--- tools/llm_perf/build_win.bat
+++ tools/llm_perf/build_win.bat
@@ -1,3 +1,28 @@
+setlocal enabledelayedexpansion
+set "TARGET_DIR=3rdparty\eigen3"
+set "DOWNLOAD_URL=!HOUMO_MODELZOO_URL!\models\qwen3\3rdparty.zip"
+set "ZIP_FILE=3rdparty.zip"
+if not exist "%TARGET_DIR%" (
+    echo Directory not found: %TARGET_DIR%
+    echo Starting to download 3rdparty.zip...
+    PowerShell -Command "(New-Object System.Net.WebClient).DownloadFile('%DOWNLOAD_URL%', '%ZIP_FILE%')"
+    if not exist "%ZIP_FILE%" (
+        echo Error: Failed to download 3rdparty.zip!
+        exit /b 1
+    )
+    echo Starting to extract 3rdparty.zip...
+    tar -xf "%ZIP_FILE%"
+    if not exist "%TARGET_DIR%" (
+        echo Error: Failed to extract 3rdparty.zip!
+        exit /b 1
+    )
+    echo Cleaning up temporary files...
+    del /f /q "%ZIP_FILE%"
+    echo 3rdparty dependencies installed successfully!
+) else (
+    echo Directory %TARGET_DIR% already exists, skipping download and extraction
+)
+
 set PROJECT_DIR=%cd%
 set HOUMO_EXAMPLES_PATH=%PROJECT_DIR%\..\..
 set TCIM_RUNTIME_DIR=D:\temp\qwen3\houmo_tcim_runtime
@@ -10,9 +35,11 @@ set BUILD_DIR=build_vs2022
 set HDPL_PLATFORM=ASIC
 
 rem c++ example
-rmdir build
+if exist "build" (rmdir /s /q build)
 md build
 cd build
 
 cmake .. -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%PROJECT_DIR%/../bin
 cmake --build . --target=install --config=%BUILD_TYPE% || echo ERROR && cd .. && exit /b
+pause
+endlocal
