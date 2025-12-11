3b6206db82e84756416352660ffda72a408938ae
xinyu.ge <xinyu.ge@houmo.ai>
2025-12-11 11:15:51 +0800

    HMSW-3014: examples supprot qwen3vl quant
    Signed-off-by: xinyu.ge <xinyu.ge@houmo.ai>
    Change-Id: Idbe94d5eaff1c95228da14bd59cb8a182f57e6ff

--- models/vllm/qwen3-vl/quant_pipeline.py
+++ models/vllm/qwen3-vl/quant_pipeline.py
@@ -11,10 +11,10 @@ from qwen_vl_utils import process_vision_info
 from safetensors.torch import load_file as load_safetensors_file
 from safetensors.torch import save_file as save_safetensors_file
 from tqdm import tqdm
-from transformers import AutoConfig, AutoProcessor
+from transformers import AutoConfig, AutoProcessor, AutoModelForImageTextToText
 
 from xh_model_zoo.xh_llm import LLMConverter
-from xh_model_zoo.xh_llm.models.qwen2_5_vl import Qwen2_5_VLConvertConfig, VisualConfig
+from xh_model_zoo.xh_llm.models.qwen3_vl import Qwen3_VLConvertConfig, VisualConfig
 
 from xhquant.api import (
     DeviceType,
@@ -57,99 +57,16 @@ def robust_file_copy(src_file, dst_path):
         print(f"An unexpected error occurred: {e}")
 
 
-def demo(model, processor):
-    from accelerate import dispatch_model, infer_auto_device_map
-    from accelerate.utils import get_balanced_memory
-
-    from xh_model_zoo.xh_llm.quarot import utils
-
-    raw_device = next(model.parameters()).device
-    # model.to(utils.DEV)
-
-    no_split_module_classes = [
-        "LlamaDecoderLayer",
-        "QuantDecoderLayer",
-        "RotateModule",
-        "SmoothModule",
-        "Qwen2DecoderLayer",
-        "Qwen2_5_VLDecoderLayer",
-    ]
-    max_memory = get_balanced_memory(
-        model, no_split_module_classes=no_split_module_classes
-    )
-    device_map = infer_auto_device_map(
-        model, max_memory=max_memory, no_split_module_classes=no_split_module_classes
-    )
-    dispatch_model(
-        model,
-        device_map=device_map,
-        offload_buffers=True,
-        offload_dir="offload",
-        state_dict=model.state_dict(),
-    )
-
-    messages = [
-        {
-            "role": "user",
-            "content": [
-                {
-                    "type": "image",
-                    "image": "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg",
-                },
-                {"type": "text", "text": "Describe this image."},
-            ],
-        }
-    ]
-
-    # Preparation for inference
-    text = processor.apply_chat_template(
-        messages, tokenize=False, add_generation_prompt=True
-    )
-    image_inputs, video_inputs = process_vision_info(messages)
-    inputs = processor(
-        text=[text],
-        images=image_inputs,
-        videos=video_inputs,
-        padding=True,
-        return_tensors="pt",
-    )
-    inputs = inputs.to("cuda")
-
-    # Inference: Generation of the output
-    generated_ids = model.generate(**inputs, max_new_tokens=512)
-    generated_ids_trimmed = [
-        out_ids[len(in_ids) :]
-        for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
-    ]
-    output_text = processor.batch_decode(
-        generated_ids_trimmed,
-        skip_special_tokens=True,
-        clean_up_tokenization_spaces=False,
-    )
-    print(output_text)
-    from accelerate.hooks import remove_hook_from_module
-
-    remove_hook_from_module(model)
-    model.to(raw_device)
-    utils.cleanup_memory()
-
-
 def quant_llm(args):
-    from transformers import Qwen2_5_VLForConditionalGeneration
-
     out_dir = Path(args.work_dir)
     hf_model_dir = args.model
 
     hf_model_path = osp.normpath(osp.abspath(args.model))
     model_name = Path(hf_model_path).name
     cfg_name = model_name
-    if not args.skip_quarot:
-        cfg_name += "_quarot"
-    if not args.skip_gptq:
-        cfg_name += "_gptq"
+    cfg_name += "_quarot"
+    cfg_name += "_gptq"
 
-    # INSERT_YOUR_CODE
-    # 增加transformers版本信息到cfg_name
     cfg_name += f"_transformers-{transformers.__version__}"
 
     work_dir = Path(out_dir) / cfg_name
@@ -159,11 +76,10 @@ def quant_llm(args):
     device = "cuda" if torch.cuda.is_available() else "cpu"
     dtype = torch.float16
 
-    native_model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
+    native_model: nn.Module = AutoModelForImageTextToText.from_pretrained(
         hf_model_dir,
         torch_dtype=dtype,
         device_map="cpu",
-        # config=config,
         trust_remote_code=True,
         attn_implementation="sdpa",
     )
@@ -175,40 +91,83 @@ def quant_llm(args):
         native_model.config.tie_word_embeddings = False
         native_model.config.torchscript = old_torchscript
 
+        native_model.config.text_config.tie_word_embeddings = False
+
     native_model.eval()
     native_model.to(dtype)
 
     processor = AutoProcessor.from_pretrained(hf_model_dir)
 
-    if args.validate:
-        demo(native_model, processor)
-
     quant_methods = []
-    if not args.skip_quarot:
-        torch.cuda.reset_peak_memory_stats()
-        quant_methods.append("quarot")
-        quant_name = "_".join(quant_methods)
-        filename = work_dir / f"{quant_name}-state-dict.safetensors"
-        if not os.path.exists(filename):
-            from xh_model_zoo.xh_llm.quarot.quantizer_utils import quarot
 
-            logger.info(msg_output_format("Start quarot quantization"))
-            native_model = quarot(native_model, device=device)
-            logger.info(msg_output_format("End quarot quantization"))
-            native_model.to(torch.float16)
-            state_dict = native_model.state_dict()
-            logger.info(msg_output_format(f"Saving checkpoint to: {filename}"))
-            # torch.save(state_dict, filename)
-            save_safetensors_file(state_dict, filename)
-            logger.info(f"Save checkpoint to: {filename}")
-        else:
-            state_dict = load_safetensors_file(filename)
-            native_model.to(torch.float16)
-            native_model.load_state_dict(state_dict)
-            logger.info(msg_output_format(f"Load state_dict from {filename}"))
+    torch.cuda.reset_peak_memory_stats()
+    quant_methods.append("quarot")
+    quant_name = "_".join(quant_methods)
+    filename = work_dir / f"{quant_name}-state-dict.safetensors"
 
-    if args.validate:
-        demo(native_model.to(torch.float32), processor)
+    if not os.path.exists(filename):
+        from xh_model_zoo.xh_llm.quarot.quantizer_utils import quarot
+
+        logger.info(msg_output_format("Start quarot quantization"))
+        native_model = quarot(native_model, device=device)
+        logger.info(msg_output_format("End quarot quantization"))
+        native_model.to(torch.float16)
+        state_dict = native_model.state_dict()
+        logger.info(msg_output_format(f"Saving checkpoint to: {filename}"))
+        # torch.save(state_dict, filename)
+        save_safetensors_file(state_dict, filename)
+        logger.info(f"Save checkpoint to: {filename}")
+    else:
+        from xh_model_zoo.xh_llm.quarot.quantizer_utils import rotation_utils
+
+        rotation_utils.fuse_layer_norms(native_model)
+        state_dict = load_safetensors_file(filename)
+        native_model.to(torch.float16)
+        native_model.load_state_dict(state_dict)
+        logger.info(msg_output_format(f"Load state_dict from {filename}"))
+
+    # if args.demo:
+    #     demo(native_model, processor, args.image_dir)
+
+    from xh_model_zoo.xh_llm.quarot.quantizer_utils import gptq
+
+    gptq_config = dict(
+        calib_dataset=args.calib_dataset,
+        calib_samples=args.calib_samples,
+        seqlen=2048,
+        w_clip=True,
+        w_bits=args.w_bits,
+        w_asym=False,
+        w_groupsize=64,
+        percdamp=0.01,
+        act_order=False,
+        int8_down_proj=False,
+        heading_gptq=args.heading_gptq,
+        w_head_bits=args.w_head_bits,
+    )
+
+    torch.cuda.reset_peak_memory_stats()
+    logger.info(msg_output_format("Start gptq quantization"))
+    quant_methods.append("gptq")
+    quant_methods.append(f"use_hession_mse_{args.use_hession_mse}")
+    quant_methods.append(f"calib_samples_{args.calib_samples}")
+    quant_methods.append(f"heading_gptq_{args.heading_gptq}")
+    layers_cache_dir = work_dir / "layers_cache"
+    layers_cache_dir.mkdir(exist_ok=True, parents=True)
+
+    native_model = gptq(
+        native_model,
+        args=args,
+        model_name=hf_model_dir,
+        **gptq_config,
+        device=device,
+        processor=processor,
+        data_files=args.data_files,
+        is_qwen3_vl=True,
+        use_hession_mse=args.use_hession_mse,
+    )
+    logger.info(msg_output_format("End gptq quantization"))
+
     consumption = torch.cuda.max_memory_allocated()
     unit = "B"
     if consumption > 1024:
@@ -219,54 +178,6 @@ def quant_llm(args):
             unit = "M"
         consumption = round(consumption, 2)
     logger.info(f"GPU memory cost for export {consumption}{unit}")
-
-    if not args.skip_gptq:
-        from xh_model_zoo.xh_llm.quarot.quantizer_utils import gptq
-
-        gptq_config = dict(
-            calib_dataset=args.calib_dataset,
-            calib_samples=args.calib_samples,
-            seqlen=2048,
-            w_clip=True,
-            w_bits=args.w_bits,
-            w_asym=False,
-            w_groupsize=64,
-            percdamp=0.01,
-            act_order=False,
-            int8_down_proj=False,
-            heading_gptq=True,
-            w_head_bits=args.w_head_bits
-        )
-
-        torch.cuda.reset_peak_memory_stats()
-        logger.info(msg_output_format("Start gptq quantization"))
-        quant_methods.append("gptq")
-        layers_cache_dir = work_dir / "layers_cache"
-        layers_cache_dir.mkdir(exist_ok=True, parents=True)
-
-        native_model = gptq(
-            native_model,
-            args=args,
-            model_name=hf_model_dir,
-            **gptq_config,
-            device=device,
-            layers_cache_dir=str(layers_cache_dir),
-            is_qwen2_5_vl=True,
-            processor=processor,
-            data_files=args.data_files,
-        )
-        logger.info(msg_output_format("End gptq quantization"))
-
-        consumption = torch.cuda.max_memory_allocated()
-        unit = "B"
-        if consumption > 1024:
-            consumption = consumption / 1024
-            unit = "k"
-            if consumption > 1024:
-                consumption = consumption / 1024
-                unit = "M"
-            consumption = round(consumption, 2)
-        logger.info(f"GPU memory cost for export {consumption}{unit}")
 
     if len(quant_methods) != 0:
         quant_name = "_".join(quant_methods)
@@ -292,17 +203,26 @@ def quant_llm(args):
         logger.info(f"Save checkpoint to: {filename}")
 
 
+def find_files_pathlib(directory):
+    path = Path(directory)
+    target_files = [
+        f.name
+        for f in path.iterdir()
+        if f.is_file() and "gptq" in f.name and "quarot" in f.name
+    ]
+    return target_files
+
+
 def export_llm(args):
-    from xh_model_zoo.xh_llm.models.qwen2_5_vl.modeling_qwen2_5_vl import (
-        Qwen2_5_VLForConditionalGeneration,
-    )
+    from xh_model_zoo.xh_llm.models.qwen3_vl import Qwen3VLForConditionalGeneration
 
     robust_file_copy("../../../data/pic/beach.jpeg", "data/images/qwen2_vl_demo.jpeg")
     hf_model_path = osp.normpath(osp.abspath(args.model))
     model_name = Path(hf_model_path).name
     target_device = DeviceType.XH2a
     quant_type = args.quant_type
-    ops=dict(MatMul=dict(
+    ops = dict(
+        MatMul=dict(
             act_scheme=dict(
                 bits=8,
                 fp_mode="sefp",
@@ -310,16 +230,23 @@ def export_llm(args):
             act_schema_2=dict(
                 bits=16,
                 fp_mode="sefp",
-            ),))
-    quant_scheme = QuantScheme(target_device=DeviceType.XH2a, quant_type=quant_type, ops=ops)
+            ),
+        )
+    )
+    quant_scheme = QuantScheme(
+        target_device=DeviceType.XH2a, quant_type=quant_type, ops=ops
+    )
     model_name = os.path.basename(args.model)
     model_dir = os.path.join(args.work_dir, "{}_quarot_gptq".format(model_name))
+    quant_weight_name = find_files_pathlib(
+        "{}_transformers-{}".format(model_dir, transformers.__version__)
+    )[0]
     quant_weight = os.path.join(
         "{}_transformers-{}".format(model_dir, transformers.__version__),
-        "quarot_gptq-state-dict.safetensors",
+        quant_weight_name,
     )
 
-    native_model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
+    native_model = Qwen3VLForConditionalGeneration.from_pretrained(
         hf_model_path,
         torch_dtype=torch.bfloat16,
         device_map="cpu",
@@ -341,7 +268,7 @@ def export_llm(args):
     processor = AutoProcessor.from_pretrained(hf_model_path)
 
     # demo(native_model, processor)
-    config = Qwen2_5_VLConvertConfig(
+    config = Qwen3_VLConvertConfig(
         batch_size=args.batch_size,
         context_length=args.context_length,
         quant_scheme=quant_scheme,
@@ -366,7 +293,7 @@ def export_llm(args):
     logger = get_root_logger()
     with TimeProfiler("convert", logger), MemoryTracker("cuda:0", "convert", logger):
         LLMConverter.from_pretrained(
-            hf_model_path, "Qwen2_5_VLForConditionalGeneration", config, work_dir
+            hf_model_path, "Qwen3VLForConditionalGeneration", config, work_dir
         )
 
 
@@ -396,6 +323,7 @@ def clean_directory(directory):
         if os.path.isdir(path):
             shutil.rmtree(path)
 
+
 def ensure_and_clean_dir(dir_path):
     if dir_path.exists():
         for item in dir_path.iterdir():
@@ -407,6 +335,7 @@ def ensure_and_clean_dir(dir_path):
     else:
         dir_path.mkdir(parents=True, exist_ok=True)
         print(f"Created directory: {dir_path}")
+
 
 def move_llm(args):
     work_dir = Path(args.work_dir)
