978a4671290f56482aeebbd9cf967adf25dd0f8b
xinyu.ge <xinyu.ge@houmo.ai>
2026-01-05 11:48:40 +0800

    HMSW-3225: add venv
    Signed-off-by: xinyu.ge <xinyu.ge@houmo.ai>
    Change-Id: I882761e684f23957e5d6fc816587fa8b2fd1a69a

--- models/llm/qwen3-30b-a3b/test.sh
+++ models/llm/qwen3-30b-a3b/test.sh
@@ -3,10 +3,12 @@ set -e
 
 STEP="all"
 MODEL_TYPE="precompiled"
+MULTI_BATCH="false"
 
 show_help() {
     echo "Usage: $0 [options]"
-    echo "  -s, --step         execution step, default is all, support: all, demo."
+    echo "  -s, --step         execution step, default is all, support: all, demo, build."
+    echo "  -b, --multi_batch  execution multibatch demo, default is false, support: true/false."
     echo "  -t, --model_type   The method for getting the compiled model, default is precompiled, support: precompiled, compile."
     echo "  -h, --help         help information"
     exit 0
@@ -16,6 +18,10 @@ while [[ $# -gt 0 ]]; do
     case $1 in
         -s|--step)
             STEP="$2"
+            shift 2
+        ;;
+        -b|--multi_batch)
+            MULTI_BATCH="$2"
             shift 2
         ;;
         -t|--model_type)
@@ -32,16 +38,55 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-houmo_target="${HOUMO_TARGET}"
-if [ -z "$houmo_target" ] || [ "$houmo_target" != "xh2" ]; then
-    echo "Only supports HOUMO_TARGET as xh2."
-    exit 1
-fi
-
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${SCRIPT_DIR}"
 
-if [ "$STEP" = "all" ] || [ "$STEP" = "demo" ]; then
+VENV_FLAG=0
+if [ -f "${SCRIPT_DIR}/requirements.txt" ]; then
+    VENV_FLAG=1
+fi
+
+dir_path="qwen3"
+if [[ "$VENV_FLAG" -eq "1" ]]; then
+    echo "⚠ Create python3 venv for ${dir_path} demo."
+    PY_EXE=$(command -v python3)
+    SITE_PACKAGES=$($PY_EXE -c "import site; print(site.getsitepackages()[0])")
+    if [[ $PY_EXE == */opt/venv* ]]; then
+        virtualenv --python=$PY_EXE --extra-search-dir=$SITE_PACKAGES $dir_path
+        VENV_PYTHON="${dir_path}/bin/python3"
+        VENV_SITE=$(${VENV_PYTHON} -c "import site; print(site.getsitepackages()[0])")
+        echo "export ORIGINAL_PYTHONPATH=\$PYTHONPATH" >> $dir_path/bin/activate  # 保存原始值
+        echo "export PYTHONPATH=${VENV_SITE}:${SITE_PACKAGES}:\$ORIGINAL_PYTHONPATH" >> $dir_path/bin/activate
+        echo "export PYTHONPATH=\$ORIGINAL_PYTHONPATH" >> $dir_path/bin/deactivate  # 恢复外部原始值
+        echo "unset ORIGINAL_PYTHONPATH" >> $dir_path/bin/deactivate  # 清除临时变量
+        sed -i 's/include-system-site-packages = true/include-system-site-packages = false/g' $dir_path/pyvenv.cfg
+    else
+        virtualenv --python=$PY_EXE --system-site-packages $dir_path
+    fi
+    source $dir_path/bin/activate
+    pip3 install -r requirements.txt
+fi
+
+PACKAGE_PATTERN=hmquant
+FOUND_PACKAGE=0
+echo "================================"
+echo "Checking python3 package: $PACKAGE_PATTERN"
+if command -v python3 &>/dev/null && command -v pip3 &>/dev/null; then
+    if pip3 list --format=columns 2>/dev/null | grep -E "^$PACKAGE_PATTERN" >/dev/null 2>&1; then
+        echo "✓ Found python3 package: $PACKAGE_PATTERN"
+        pip3 list --format=columns 2>/dev/null | grep -E "^$PACKAGE_PATTERN" | while read -r line; do
+            echo "  - $line"
+        done
+        FOUND_PACKAGE=1
+    else
+        echo "✗ Not found package: $PACKAGE_PATTERN"
+    fi
+else
+    echo "⚠ Not found python3 or pip3."
+    exit 0
+fi
+
+if [ "$STEP" = "all" ] || [ "$STEP" = "build" ]; then
     if [[ "$MODEL_TYPE" == "precompiled" ]]; then
         echo "Download precompiled model."
         python3 get_model.py --type hmm
@@ -82,39 +127,7 @@ if [ "$STEP" = "all" ] || [ "$STEP" = "demo" ]; then
         if [[ "$FOUND_PACKAGE" -eq 1 && "$FOUND_GPU" -eq 1 ]]; then
             echo "Start to quant and compile model."
             python3 get_model.py --type raw
-
-            VENV_FLAG=0
-            if [ -f "${SCRIPT_DIR}/requirements_ptq.txt" ]; then
-                VENV_FLAG=1
-            fi
-            dir_path="qwen3moe_venv"
-            if [[ "$VENV_FLAG" -eq "1" ]]; then
-                echo "⚠ Create python3 venv for ${dir_path} demo."
-                PY_EXE=$(command -v python3)
-                SITE_PACKAGES=$($PY_EXE -c "import site; print(site.getsitepackages()[0])")
-                if [[ $PY_EXE == */opt/venv* ]]; then
-                    virtualenv --python=$PY_EXE --extra-search-dir=$SITE_PACKAGES $dir_path
-                    VENV_PYTHON="${dir_path}/bin/python3"
-                    VENV_SITE=$(${VENV_PYTHON} -c "import site; print(site.getsitepackages()[0])")
-                    echo "export ORIGINAL_PYTHONPATH=\$PYTHONPATH" >> $dir_path/bin/activate  # 保存原始值
-                    echo "export PYTHONPATH=${VENV_SITE}:${SITE_PACKAGES}:\$ORIGINAL_PYTHONPATH" >> $dir_path/bin/activate
-                    echo "export PYTHONPATH=\$ORIGINAL_PYTHONPATH" >> $dir_path/bin/deactivate  # 恢复外部原始值
-                    echo "unset ORIGINAL_PYTHONPATH" >> $dir_path/bin/deactivate  # 清除临时变量
-                    sed -i 's/include-system-site-packages = true/include-system-site-packages = false/g' $dir_path/pyvenv.cfg
-                else
-                    virtualenv --python=$PY_EXE --system-site-packages $dir_path
-                fi
-                source $dir_path/bin/activate
-                pip3 install -r requirements_ptq.txt
-            fi
-            if [[ ! -d "$dir_path/lib/python3.12/site-packages/distutils" ]]; then
-                ln -s /opt/venv/houmo/lib/python3.12/site-packages/setuptools/_distutils $dir_path/lib/python3.12/site-packages/distutils
-            fi
             python3 ptq.py
-            if [[ "$VENV_FLAG" -eq "1" ]]; then
-                deactivate
-                rm -rf $dir_path
-            fi
             python3 build.py
         else
             echo "✗ Not support model quantization and compilation."
@@ -124,6 +137,16 @@ if [ "$STEP" = "all" ] || [ "$STEP" = "demo" ]; then
 fi
 
 if [ "$STEP" = "all" ] || [ "$STEP" = "demo" ]; then
-    echo "Execute demo."
-    python3 demo.py
+    if [ "$MULTI_BATCH" = "false" ]; then
+        echo "Execute demo."
+        python3 demo.py
+    else
+        echo "Execute multi-batch demo with batch size: $MULTI_BATCH"
+        python3 demo_multibatch.py --forbid_flush
+    fi
+fi
+
+if [[ "$VENV_FLAG" -eq "1" ]]; then
+    deactivate
+    rm -rf $dir_path
 fi
