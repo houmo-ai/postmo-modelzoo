b95e72002a79dc6fbc7b9c7e205c85663e477475
yaozhen.liu <yaozhen.liu@houmo.ai>
2026-01-20 12:42:40 +0800

    HMSW-3409 : support monitor interval set and summary show.
    Change-Id: Ia5b3b97c5e1c2324920ecf68d4454945a83832aa
    (cherry picked from commit 098698de19fad556d4226ce3c713ef7e823339cf)

--- tools/llm_perf/utils.h
+++ tools/llm_perf/utils.h
@@ -91,6 +91,7 @@ static void HelpUsage(char* argv[]) {
          "  --batch         NUM       if multibatch model only xh2 support!\n"
          "  --no_warm_up              disable warm up!\n"
          "  --LazyMode                enable lazy mode!\n"
+         "  --interval                device 0 monitor interval!\n"
          "  -h, --help                show help message\n";
 }
 
