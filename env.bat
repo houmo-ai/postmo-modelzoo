6e30ecca725a819071638ce1c21162844d7f8247
yaozhen.liu <yaozhen.liu@houmo.ai>
2026-01-29 16:08:52 +0800

    HMSW-3511 : remove --clear from env.bat
    Change-Id: Ic8915f3fc593101397afabe00242f3703dae8578
    (cherry picked from commit a1b2c04691a988411ceb2798b719107c6a55a537)

--- env.bat
+++ env.bat
@@ -5,11 +5,10 @@ if defined PYTHON_PATH (set PYTHON_DIR="%PYTHON_PATH%")
 set argC=0
 for %%x in (%*) do set /a argC+=1
 if %argC% gtr 1 (
-    echo Error: Only 0 arguments or 1 '--set'/'--clear'/'--reset'/'--help'/'-h' argument allowed
+    echo Error: Only 0 arguments or 1 '--set'/'--reset'/'--help'/'-h' argument allowed
     echo Usage:
     echo   %0 --set
     echo   %0 --reset
-    echo   %0 --clear
     exit /b 1
 )
 
@@ -17,24 +16,19 @@ if %argC% gtr 1 (
 if %argC% equ 1 (
     if "%1"=="--reset" (
         echo ===============Reset Win Envs===============
-    ) else if "%1"=="--clear" (
-        echo ===============Clear Win Envs===============
     ) else if "%1"=="--set" (
         echo ===============Set Win Envs===============
     ) else if "%1"=="--help" (
         echo 'env.bat --set' set environments
-        echo 'env.bat --clear' clear environments 
         echo 'env.bat --reset' reset environments
         exit /b 1
     ) else if "%1"=="-h" (
         echo 'env.bat --set' set environments
-        echo 'env.bat --clear' clear environments 
         echo 'env.bat --reset' reset environments
         exit /b 1
     ) else (
         echo Error: Invalid argument '%1'
         echo 'env.bat --set' set environments
-        echo 'env.bat --clear' clear environments 
         echo 'env.bat --reset' reset environments
         exit /b 1
     )
@@ -43,7 +37,6 @@ if %argC% equ 1 (
 :: Execute corresponding python command
 if %argC% equ 0 (
     echo 'env.bat --set' set environments
-    echo 'env.bat --clear' clear environments 
     echo 'env.bat --reset' reset environments
     exit /b 1
 ) else (
