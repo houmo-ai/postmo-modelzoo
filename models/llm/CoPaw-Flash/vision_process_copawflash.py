7754890f1194666787f422ccc4bb0db45fbb0720
nan.xu <nan.xu@houmo.ai>
2026-05-07 10:48:02 +0800

    HMSW-4099: repair copyright
    Change-Id: I0ba9b0e653bdb3739c49e725c74344be27988d22

--- models/llm/CoPaw-Flash/vision_process_copawflash.py
+++ models/llm/CoPaw-Flash/vision_process_copawflash.py
@@ -1,17 +1,3 @@
-# Copyright 2026 HOUMO AI. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 # ----------------------------------------------------------------              
 # This file contains code modified from the Qwen3-VL project:
 # Source: https://github.com/QwenLM/Qwen3-VL/blob/main/qwen-vl-utils/src/qwen_vl_utils/vision_process.py
